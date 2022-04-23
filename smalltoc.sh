2e4656cd1443b53249826413c20e6a5c08d0f482
Adam Oller <81510507+Adamocho@users.noreply.github.com>
2022-04-23 17:13:43 +0200

    Add Options
    Create case block with some options:
    installing/uninstalling
    help

--- smalltoc.sh
+++ smalltoc.sh
@@ -1,26 +1,60 @@
 #!/bin/sh
 
-before=2
+INSTALL_PATH="/usr/bin/smalltoc"
 
-printf "## Table of content\n"
+generate_toc() {
+    before=2
 
-while read -r line
-do
-    line=$(echo "$line" | grep -E "#{2,}[[:space:]][[:alnum:]]")
-    after=$(echo "$line" | cut -d ' ' -f 1 | tr -d "[:space:]" | wc -m)
+    printf "## Table of content\n"
 
-    [ -z "$line" ] && continue
+    while read -r line
+    do
+        line=$(echo "$line" | grep -E "#{2,}[[:space:]][[:alnum:]]")
+        after=$(echo "$line" | cut -d ' ' -f 1 | tr -d "[:space:]" | wc -m)
 
-    text=$(echo "$line" | cut -d ' ' -f 1 --complement)
-    link=$(echo "$text" | tr ' ' '-' | tr '[:upper:]' '[:lower:]')
+        [ -z "$line" ] && continue
 
-    {
-        i=2
-        while [ $i -lt "$after" ]; do
-            printf "%b" "\t"
+        text=$(echo "$line" | cut -d ' ' -f 1 --complement)
+        link=$(echo "$text" | tr ' ' '-' | tr '[:upper:]' '[:lower:]')
 
-            i=$((i + 1))
-        done
-        printf "%s [%s](#%s)\n" "-" "$text" "$link"
-    }
-done
+        {
+            i=2
+            while [ $i -lt "$after" ]; do
+                printf "%b" "\t"
+
+                i=$((i + 1))
+            done
+            printf "%s [%s](#%s)\n" "-" "$text" "$link"
+        }
+    done
+}
+
+case $1 in
+    '')
+        generate_toc
+        ;;
+    'install' | 'add')
+        (set -x; sudo cp "$0" $INSTALL_PATH)
+        ;;
+    'uninstall' | 'remove')
+        (set -x; sudo rm -i $INSTALL_PATH)
+        ;;
+    '-h' | '--help')
+        echo "smalltoc - Lightweight Table of content genrator
+        Usage:
+            smalltoc [Options]
+
+            NOTE: The script is meant to read from pipe!
+
+            OPTIONS
+
+            install/add - add script to \$PATH
+
+            uninstall/remove - remove script from \$PATH
+
+            -h/--help - show help"
+        ;;
+    *)
+        echo "Wrong arguments given: $1"
+        ;;
+esac
