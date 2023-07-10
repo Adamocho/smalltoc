62a5d626ce4ce61ac5733efce1c4a81123279d68
Adamocho <81510507+Adamocho@users.noreply.github.com>
2023-07-10 23:35:24 +0200

    Code cleanup: - Change variable names - Fix error-gen situations (number eval without the '$' sign) - Add proper comments

--- smalltoc.sh
+++ smalltoc.sh
@@ -1,68 +1,95 @@
 #!/bin/sh
 
 INSTALL_PATH="/usr/bin/smalltoc"
+OUTPUT_DIR="$HOME/.cache/smalltoc"
+OUTPUT_FILE="$OUTPUT_DIR/README.md"
 
+# It uses 'printf' but it is captured in the parse_args() function, so nothing is printed.
 generate_toc() {
-    while IFS= read -r line
+    # When there are still lines to read
+    # Store it in the $line variable 
+    while lines= read -r line
     do
+        # Reuse $line variable - check for containing '#' in a specific order.
         line=$(echo "$line" | grep -E "#{2,}[[:space:]][[:alnum:]]")
 
+        # If it is non-zero, proceed
         [ -z "$line" ] && continue
 
+        # Skip, if it contains 'Table of Content' (case insensitive)
         [ "$(echo "$line" | grep -Ei '##[[:space:]]*table[[:space:]]*of[[:space:]]*content.*')" ] && continue
 
-        text=$(echo "$line" | cut -d ' ' -f 1 --complement)
-        link=$(echo "$text" | xargs | tr ' ' '-' | tr '[:upper:]' '[:lower:]')
-        num_tabs=$(echo "$line" | cut -d ' ' -f 1 | tr -d "[:space:]" | wc -m)
+        # Get text for link (basically copy the header without hashtags)
+        text_part=$(echo "$line" | cut -d ' ' -f 1 --complement)
+        
+        # Get the link part, which looks like: (example-section)
+        link_part=$(echo "$text_part" | xargs | tr ' ' '-' | tr '[:upper:]' '[:lower:]')
 
-        i=0
-        num_tabs=$((num_tabs - 2))
+        # Calculate number of tabulators...
+        number_of_tabs=$(echo "$line" | cut -d ' ' -f 1 | tr -d "[:space:]" | wc -m)
+        tabs_iterator=0
+        number_of_tabs=$(( $number_of_tabs - 2 ))
 
-        while [ $i -lt "$num_tabs" ]; do
+        # ...and shift the line that many times
+        while [ $tabs_iterator -lt "$number_of_tabs" ]; do
+            # Add tabulator (\t didn't work for some reason)
             printf "%s" "    "
-            i=$((i + 1))
+            tabs_iterator=$(( $tabs_iterator + 1 ))
         done
 
-        printf "%s [%s](#%s)\n" "-" "$text" "$link"
+        # Create link to the section
+        printf "%s [%s](#%s)\n" "-" "$text_part" "$link_part"
+
+    # When done, shift the arguments one to the left (discarding the one used recently).
     done < "$1"
 }
 
 parse_args() {
-    for arg in "$@"
+    # Generate TOC for each file given.
+    for file in "$@"
         do
         [ -e "$1" ] && printf "Generating TOC for %s:\n" "$1"
 
-        OUTPUT_DIR="$HOME/.cache/smalltoc"
-        OUTPUT_FILE="$OUTPUT_DIR/README.md"
+        # Create the directory if needed (no error otherwise).
         mkdir -p "$OUTPUT_DIR"
 
-        printf "The file is saved in %s\n" "$OUTPUT_FILE"
+        # Generate the ToC for the current file.
+        table_of_content=$( printf "## Table of Content\n%s" "$(generate_toc $file)" )
 
-        toc="$( printf "## Table of Content\n%s" "$(generate_toc "$arg")" )"
-        htwos="$( grep -Enim 2 '#{2,}' "$arg" )"
-        htwos_num="$( grep -Enim 2 -c '#{2,}' "$arg" )"
+        # Locate the first double-hashtag occurence
+        hashtag_twice=$( grep -Enim 2 '#{2,}' "$file" )
 
-        if [ "$htwos_num" -eq 0 ]
+        # And the number of them
+        number_of_hashtag_twice=$( grep -Enim 2 -c '#{2,}' "$file" )
+
+        # If there are none found, just paste the toc to output
+        if [ "$number_of_hashtag_twice" -eq 0 ]
         then
-            cat "$arg" > "$OUTPUT_FILE"
-            printf "\n%s" "$toc" >> "$OUTPUT_FILE"
+            cat "$file" > "$OUTPUT_FILE"
+            printf "\n%s" "$table_of_content" >> "$OUTPUT_FILE"
+
             continue
         fi
 
-        a=$(echo "$htwos" | head -1)
-        b=$(echo "$htwos" | tail -1)
+        # Locate where to put TOC in
+        top_part=$(echo "$hashtag_twice" | head -1)
+        bottom_part=$(echo "$hashtag_twice" | tail -1)
 
-        [ "$(echo "$a" | grep -Ei 'table[[:space:]]*of[[:space:]]*content.*' )" ] || b="$a"
+        [ "$(echo "$top_part" | grep -Ei 'table[[:space:]]*of[[:space:]]*content.*' )" ] || bottom_part="$top_part"
 
-        a="$(echo "$a" | cut -d : -f 1 )"
-        b="$(echo "$b" | cut -d : -f 1 )"
+        top_part=$(echo "$top_part" | cut -d : -f 1 )
+        bottom_part=$(echo "$bottom_part" | cut -d : -f 1 )
 
-        head -"$(( a - 1 ))" "$arg" > "$OUTPUT_FILE"
+        # Merge TOC into file
+        # Start
+        head -"$(( $top_part - 1 ))" "$file" > "$OUTPUT_FILE"
+        # Middle - add TOC
+        printf "\n%s\n\n" "$table_of_content" >> "$OUTPUT_FILE"
+        # End
+        tail -n +"$bottom_part" "$file" >> "$OUTPUT_FILE"
 
-        printf "\n%s\n\n" "$toc" >> "$OUTPUT_FILE"
-
-        tail -n +"$b" "$arg" >> "$OUTPUT_FILE"
-
+        # Debug for the end-user
+        printf "The file is saved in %s\n" "$OUTPUT_FILE"
     done
 }
 
